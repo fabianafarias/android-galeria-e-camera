c8a74ae87cdd5d2e7a3047abb3e99bf3be6100cc
fabiana farias <farias.fabianam@gmail.com>
2021-05-18 16:06:04 -0300

    Função pickImageFromGallery e conclusão da aplicação

--- app/src/main/java/com/fabianafarias/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/fotosbootcamp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fabianafarias.fotosbootcamp
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -29,6 +31,19 @@ class MainActivity : AppCompatActivity() {
         else {
                 pickImageFromGallery()
             }
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            image_view.setImageURI(data?.data)
         }
     }
 
