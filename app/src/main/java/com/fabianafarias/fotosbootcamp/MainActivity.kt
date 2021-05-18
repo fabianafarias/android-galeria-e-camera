8fa8f08fbaaee46e23effc6e84de2a751d3bc3ff
fabiana farias <farias.fabianam@gmail.com>
2021-05-18 15:54:44 -0300

    Desenvolvimento inicial da MainActivity e Permissões

--- app/src/main/java/com/fabianafarias/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/fotosbootcamp/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.fabianafarias.fotosbootcamp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pick_button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    val permission =
+                        kotlin.arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+                else {
+                pickImageFromGallery()
+            }
+        }
+        else {
+                pickImageFromGallery()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray
+    ) {
+       when(requestCode) {
+           PERMISSION_CODE -> {
+               if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   pickImageFromGallery()
+               }
+               else {
+                   Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show()
+               }
+           }
+       }
+    }
+
+    companion object {
+        private val PERMISSION_CODE = 1000
+        private val IMAGE_PICK_CODE = 1001
     }
 }
