7eb17417ed37836ed2808c5d48e9706ec87d7957
fabiana farias <farias.fabianam@gmail.com>
2021-05-18 19:09:42 -0300

    Função openCamera e conclusão da aplicação

--- app/src/main/java/com/fabianafarias/camerabootcamp/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/camerabootcamp/MainActivity.kt
@@ -2,20 +2,26 @@ package com.fabianafarias.camerabootcamp
 
 import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var image_uri: Uri? = null
+
     companion object {
         private val PERMISSION_CODE_IMAGE_PICK = 1000
         private val IMAGE_PICK_CODE = 1001
         private val PERMISSION_CODE_CAMERA_CAPTURE = 2000
+        private val OPEN_CAMERA_CODE = 2001
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,10 +82,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             PERMISSION_CODE_CAMERA_CAPTURE -> {
-                if (grantResults.size > 1) &&
+                if (grantResults.size > 1 &&
                         grantResults[0] == PackageManager.PERMISSION_GRANTED &&
                         grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                            openCamera()
+
+                    openCamera()
                 }
                 else {
                     Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show()
@@ -95,10 +102,25 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Nova foto")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Imagem capturada pela câmera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+
+        startActivityForResult(cameraIntent, OPEN_CAMERA_CODE)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             image_view.setImageURI(data?.data)
+        }
+        if (resultCode == Activity.RESULT_OK && requestCode == OPEN_CAMERA_CODE) {
+            image_view.setImageURI(image_uri)
         }
     }
 
